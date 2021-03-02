cda98d755bec978ec8bffbbc38d65d0527e01a01
Gaurav Kumar <79919528+GKkumar13@users.noreply.github.com>
2021-03-02 20:22:38 +0530

    Create Calculator.rb

---
+++ Calculator.rb
@@ -0,0 +1,23 @@
+puts "Enter your choice: "
+
+chChoice = gets.chomp
+
+puts "Enter first operand: "
+
+inOperand1 = gets.chomp.to_i
+
+puts "Enter second operand: "
+
+inOperand2 = gets.chomp.to_i
+
+case chChoice
+
+  when "Add"
+      inResult = inOperand1 + inOperand2
+      
+  else
+      inResult = 0
+      
+end
+
+puts "Result is : #{inResult}"
